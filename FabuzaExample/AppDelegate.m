6c4e75c81b7782d35a624fc8e7798db62890a978
Vladimir Vodolazkiy <vloldemarus@gmail.com>
2018-12-20 17:56:55 +0300

    обнволен фреймворк и код тестовго примера.

--- FabuzaExample/AppDelegate.m
+++ FabuzaExample/AppDelegate.m
@@ -16,7 +16,7 @@
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-//    [[FZTestEngine instance] checkActiveTest];
+    [[FZTestEngine instance] checkActiveTest];
 	
     return YES;
 }
